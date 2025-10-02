d49c0ca92fa75ecca8f7a5a3e537af1ad387e771
Viiperlol <suhaybsheikh@hotmail.com>
2025-10-02 15:05:56 +0800

    commit

--- main.py
+++ main.py
@@ -63,12 +63,18 @@ class BlogPost(db.Model):
     body: Mapped[str] = mapped_column(Text, nullable=False)
     img_url: Mapped[str] = mapped_column(String(250), nullable=False)
 
+    comments = relationship("Comment", back_populates="post")
+
 class Comment(db.Model):
     __tablename__ = "comments"
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     text: Mapped[str] = mapped_column(Text, nullable=False)
+
     author_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("User.id"))
     comment_author = relationship("User", back_populates="comments")
+
+    post_id: Mapped[int] = mapped_column(Integer, db.ForeignKey("blog_posts.id"))
+    post = relationship("BlogPost", back_populates="comments")
 
 with app.app_context():
     db.create_all()
